9ecb7612c0d591e3a301bead3e2d82f44dbdb3e1
Bbi Sharkk <superdailydata@gmail.com>
2025-02-01 11:54:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<?php
-// Chuyển hướng trình duyệt đến trang https://vtee.dev/
-header("Location: https://vtee.dev/");
-
-// Dừng thực thi script sau khi chuyển hướng
-exit();
-?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chuyển hướng đến vtee.dev</title>
+    <script>
+        // Khi trang được tải, chuyển hướng đến trang https://vtee.dev/
+        window.location.href = "https://vtee.dev/";
+    </script>
+</head>
+<body>
+    <p>Nếu trang không chuyển hướng tự động, hãy <a href="https://vtee.dev/">nhấp vào đây</a>.</p>
+</body>
+</html>
